75341b1f70e426562aa076e49e34188dbe0beb19
Lacatis, Danuta Tina <danatina.dt@gmail.com>
2023-11-13 12:39:30 +0100

    create new app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123456789",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987654321",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
